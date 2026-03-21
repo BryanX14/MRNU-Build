913c6ec8750dbebe599c6affdf7317e473643262
Bryan Greig <bryangreig@Bryans-MacBook-Pro.local>
2026-03-20 21:49:09 -0700

    Initial Commit

---
+++ src/_data/client.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "Maple Ridge New and Used Building Materials INC.",
+    email: "mrnu@shaw.ca",
+    phoneForTel: "604-380-2111",
+    phoneFormatted: "(604) 380-2111",
+    address: {
+        lineOne: "23332 River Road",
+        city: "Maple Ridge",
+        state: "BC",
+        zip: "V2W 1B6",
+        country: "CA",
+        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+    },
+    socials: {
+        facebook: "https://www.facebook.com/",
+        instagram: "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    domain: "https://www.example.com",
+    // Passing the isProduction variable for use in HTML templates
+    isProduction: process.env.ELEVENTY_ENV === "PROD",
+};
